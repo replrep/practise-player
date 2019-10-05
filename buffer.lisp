e6476c46d2f5deb1ac090a504b90129e1f06ad0c
Claus Brunzema <mail@cbrunzema.de>
2019-10-05 12:43:01 +0200

    fix type errors

--- buffer.lisp
+++ buffer.lisp
@@ -20,8 +20,7 @@
     (setf (buffer-lock buffer) (make-lock))
     (setf (buffer-condition-var buffer) (make-condition-variable))
     (setf (buffer-size buffer) (1+ size))
-    (setf (buffer-data buffer) (make-array (list (1+ size))
-                                           :element-type 'single-float))
+    (setf (buffer-data buffer) (make-array (list (1+ size))))
     (setf (buffer-source-callback buffer) source-callback)
     buffer))
 
@@ -48,8 +47,7 @@
          (data (buffer-data buffer))
          (size (buffer-size buffer))
          (deliver (min len (the fixnum (buffer-read-avail buffer)))))
-    (declare (fixnum read-index size deliver)
-             (simple-vector data))
+    (declare (fixnum read-index size deliver))
     (loop for i below deliver do
          (funcall callback (svref data read-index))
          (setf read-index (mod (1+ read-index) size)))
@@ -62,11 +60,11 @@
   (setf (buffer-thread buffer)
         (make-thread
          (lambda ()
-           (let* ((data (buffer-data buffer))
-                  (size (buffer-size buffer))
-                  (callback (buffer-source-callback buffer))
-                  (lock (buffer-lock buffer))
-                  (condition-var (buffer-condition-var buffer)))
+           (let ((data (buffer-data buffer))
+                 (size (buffer-size buffer))
+                 (callback (buffer-source-callback buffer))
+                 (lock (buffer-lock buffer))
+                 (condition-var (buffer-condition-var buffer)))
              (loop while (buffer-thread-keep-running buffer) do
                   (loop while (plusp (buffer-write-avail buffer)) do
                        (let* ((write-index (buffer-write-index buffer))
