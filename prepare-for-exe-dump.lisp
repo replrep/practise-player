f8fa46e7dd472eaf528315c912e7ee5348438a5e
Claus Brunzema <mail@cbrunzema.de>
2020-05-03 20:06:49 +0200

    improve compiler/executable verbosity

--- prepare-for-exe-dump.lisp
+++ prepare-for-exe-dump.lisp
@@ -1,6 +1,14 @@
+(setq *load-verbose* nil)
+(setq *compile-verbose* nil)
+(setf *invoke-debugger-hook*
+          (lambda (condition hook)
+            (declare (ignore hook))
+            (format *error-output* "Error: ~A~%" condition)
+            (sb-ext:exit)))
+(sb-ext:disable-debugger)
+
 (load (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname)))
 (require 'asdf)
 (push (truename ".") asdf:*central-registry*)
 (asdf:load-system :practise-player)
 
-(sb-ext:disable-debugger)
